2ac091c896500f04fd7d525fc334f92d8d3ff7b1
jaymuk <jaymuk.business@gmail.com>
2015-04-14 11:41:00 +0100

    added power saving test

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,7 +5,7 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  describe('it has a temperature', function() {
+  describe('has a temperature', function() {
 
     it('that starts at 20 degrees', function () {
       expect(thermostat.temperature).toEqual(20);
@@ -24,22 +24,26 @@ describe('Thermostat', function() {
     it('with a minimum value of 10', function() {
       thermostat.temperature = 10;
 
-      expect(function() { thermostat.decreaseTemp() }).toThrow(new Error('Sorry, cannot go lower than 10'))
+      expect(function() {
+        thermostat.decreaseTemp();
+       }).toThrow(new Error('Sorry, cannot go lower than 10'));
     });
 
     it('with a maximum value of 32', function() {
       thermostat.temperature = 32;
 
-      expect(function() { thermostat.increaseTemp()}).toThrow(new Error('Sorry, cannot go higher than 32'))
+      expect(function() {
+        thermostat.increaseTemp();
+      }).toThrow(new Error('Sorry, cannot go higher than 32'));
     });
 
-  // describe('it has a power saving mode', function() {
+  describe('has a power saving mode', function() {
 
-  //   it('', function() {
+    it('that is on by default', function() {
+      expect(thermostat.powerSave).toEqual(true);
+    });
 
-  //   });
-
-  // });
+  });
 
   });
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,13 +3,17 @@ var Thermostat = function() {
 };
 
 Thermostat.prototype.increaseTemp = function() {
+  if(this.temperature === 32){
+    throw new Error('Sorry, cannot go higher than 32');
+  }
+
   this.temperature ++;
 };
 
 Thermostat.prototype.decreaseTemp = function() {
   if(this.temperature === 10) {
     throw new Error('Sorry, cannot go lower than 10');
-  };
+  }
 
   this.temperature --;
 };
