912f2e1ceca596829f51f39c0b9475cfa8788804
jaymuk <jaymuk.business@gmail.com>
2015-04-14 10:35:13 +0100

    added temp increase function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,11 +1,20 @@
 describe('Thermostat', function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
   describe('it has a temperature', function() {
 
     it('that starts at 20 degrees', function () {
-      var thermostat = new Thermostat();
 
       expect(thermostat.temperature).toEqual(20);
+    });
+
+    it('that can increase', function() {
+      thermostat.increaseTemp();
+      expect(thermostat.temperature).toEqual(21);
     });
 
   });
