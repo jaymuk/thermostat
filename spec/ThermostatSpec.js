a20cb6ae2e100f27aa11d651cf9436fcae2d10d7
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-04-14 10:48:39 +0100

    adds abiluty to increase temp and a test to for decreasing temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,13 +8,17 @@ describe('Thermostat', function() {
   describe('it has a temperature', function() {
 
     it('that starts at 20 degrees', function () {
-
       expect(thermostat.temperature).toEqual(20);
     });
 
     it('that can increase', function() {
       thermostat.increaseTemp();
       expect(thermostat.temperature).toEqual(21);
+    });
+
+    it('that can decrease', function() {
+      thermostat.decreaseTemp();
+      expect(thermostat.temperature).toEqual(19);
     });
 
   });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,3 +2,7 @@ var Thermostat = function() {
   this.temperature = 20;
 };
 
+Thermostat.prototype.increaseTemp = function() {
+  this.temperature ++;
+};
+
